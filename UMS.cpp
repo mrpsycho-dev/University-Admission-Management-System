e42acba8aecc54b57d9f79cab3cb32031f4a83fd
Muhammad Abdullah <muhammadabdullahnazir6@gmail.com>
2026-03-30 20:43:34 +0500

    update student record by name, added 2 students data

--- UMS.cpp
+++ UMS.cpp
@@ -7,16 +7,16 @@ int main()
 {
 
   int totalStudents = 1000;
-  int index = 0;
+  int index = 2;
   int loginAttempt = 0;
 
   // Data structures
-  string namesArr[totalStudents];
-  string pref1Arr[totalStudents];
-  string pref2Arr[totalStudents];
-  string pref3Arr[totalStudents];
-  int ageArr[totalStudents];
-  float matricArr[totalStudents], interArr[totalStudents], ecatArr[totalStudents], aggregateArr[totalStudents];
+  string namesArr[totalStudents] = {"ali", "ammar"};
+  string pref1Arr[totalStudents] = {"CS", "CS"};
+  string pref2Arr[totalStudents] = {"SE", "IT"};
+  string pref3Arr[totalStudents] = {"IT", "SE"};
+  int ageArr[totalStudents] = {19, 20};
+  float matricArr[totalStudents] = {1090, 1119}, interArr[totalStudents] = {1119, 1110}, ecatArr[totalStudents] = {290, 320}, aggregateArr[totalStudents];
 
   while (true)
   {
@@ -63,7 +63,7 @@ int main()
           2.  Search Student
           3.  Update Student Record
           4.  Generate Merit List
-          5.  Exit
+          5.  Logout
               Choose the option:  
           )";
 
@@ -104,6 +104,69 @@ int main()
             else if (adminOption == 3)
             {
               // update student record
+              cout << "\nEnter the name of student you want to update record: ";
+              string name;
+              cin >> name;
+              bool found = false;
+              for (int i = 0; i < index; i++)
+              {
+                cout << "------------Old record: ------------";
+                cout << "Name\tAge\tMatric\tFSC\tECAT\tP1\tP2\tP3\n";
+                if (namesArr[i] == name)
+                {
+                  cout << namesArr[i] << "\t" << ageArr[i] << "\t"
+                       << matricArr[i] << "\t" << interArr[i] << "\t" << ecatArr[i] << "\t" << pref1Arr[i] << "\t" << pref2Arr[i] << "\t" << pref3Arr[i] << "\t\n";
+                  found = true;
+                }
+              }
+              if (found)
+              {
+                cout << "Enter new record of the student: ";
+                cout << "Enter your name: ";
+                string name;
+                cin >> name;
+                cout << "Enter age: ";
+                int age;
+                cin >> age;
+                cout << "Enter Matric marks: ";
+                float matric;
+                cin >> matric;
+                cout << "Enter FSC marks: ";
+                float inter;
+                cin >> inter;
+                cout << "Enter ECAT marks: ";
+                float ecat;
+                cin >> ecat;
+                cout << "Enter CS, SE, IT as your preferences\n";
+                string pref1, pref2, pref3;
+                // while (true)
+                // {
+                // }
+                cout << "Enter your 1st preference: ";
+                cin >> pref1;
+                cout << "Enter your 2nd preference: ";
+                cin >> pref2;
+                cout << "Enter your 3rd preference: ";
+                cin >> pref3;
+
+                namesArr[index] = name;
+                ageArr[index] = age;
+                matricArr[index] = matric;
+                interArr[index] = inter;
+                ecatArr[index] = ecat;
+                pref1Arr[index] = pref1;
+                pref2Arr[index] = pref2;
+                pref3Arr[index] = pref3;
+                // index++;
+                cout << "Your data has been saved.\n";
+
+                cout << "Press any key to continue...";
+                getch();
+              }
+              else if (!found)
+              {
+                cout << "Record not found.";
+              }
             }
             else if (adminOption == 4)
             {
@@ -112,12 +175,13 @@ int main()
             else if (adminOption == 5)
             {
               // exit
+              break;
             }
             else
             {
               cout << "Invalid Option.";
             }
-            cout << "Press any key to continue...";
+            cout << "\nPress any key to continue...";
             getch();
             // break;
           }
@@ -127,7 +191,7 @@ int main()
           cout << "Username or Password is incorrect! \n";
         }
       }
-      cout << "Press any key to continue...";
+      cout << "\nPress any key to continue...";
       getch();
     }
     else if (userOption == 2)
